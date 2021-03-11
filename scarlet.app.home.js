a43411cc29968894df4c7a6a8017cc8a6a6e9bc9
Tvbuddies <64229309+Tvbuddies@users.noreply.github.com>
2021-03-11 11:36:50 +0400

    Alert Update

--- scarlet.app.home.js
+++ scarlet.app.home.js
@@ -1,3 +1,4 @@
 function errload(){
   alert("This Site does not support Mobile Resolutions as of Yet; We reccomened visiting on PC for optimal experience");
+  alert("Mobile optimization is on Our Road map Please Wait Until!");
 }
