51ee8e08206fb3f1fc4db29c24ff5682929959d5
Tvbuddies <64229309+Tvbuddies@users.noreply.github.com>
2021-03-11 11:37:55 +0400

    fix

--- scarlet.app.home.js
+++ scarlet.app.home.js
@@ -1,4 +1,4 @@
 function errload(){
   alert("This Site does not support Mobile Resolutions as of Yet; We reccomened visiting on PC for optimal experience");
-  alert("Mobile optimization is on Our Road map Please Wait Until!");
+  alert("Mobile optimization is on Our Roadmap Please Wait Until We Update Our Site =)!");
 }
